5a93d8438f8b86ebe347fa37f820f009c9c672bd
Maksim Yarotskiy <maksimyarotskiy08@gmail.com>
2024-12-21 11:56:58 +0300

    Initial commit

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,18 @@
+// src/components/TodoItem.jsx
+import React from 'react';
+
+function TodoItem({ todo, toggleTodo, deleteTodo }) {
+  return (
+    <div>
+      <input
+        type="checkbox"
+        checked={todo.completed}
+        onChange={() => toggleTodo(todo.id)}
+      />
+      <span>{todo.title}</span>
+      <button onClick={() => deleteTodo(todo.id)}>Удалить</button>
+    </div>
+  );
+}
+
+export default TodoItem;
